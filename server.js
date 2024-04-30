d7d13c51b1c0df83df25f4686fb85cbc3c30b306
unknown <youyu00022@gmail.com>
2024-04-30 18:42:04 -0500

    updated so that server.js connects to the database.

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const db = mysql.createConnection(
         host: 'localhost',
         user: 'root',
         password: 'rootroot',
-        database: '',
+        database: 'management_db',
     },
     console.log(`Connected to the management Database`)
 );
